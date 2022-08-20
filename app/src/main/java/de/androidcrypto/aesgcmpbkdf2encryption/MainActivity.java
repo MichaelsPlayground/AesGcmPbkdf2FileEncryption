9b2bd67b024df9a506b3488f8a8680cdcc65508b
FlutterCrypto <fluttercrypto@gmx.de>
2022-08-20 11:24:45 +0200

    updated

--- app/src/main/java/de/androidcrypto/aesgcmpbkdf2encryption/MainActivity.java
+++ app/src/main/java/de/androidcrypto/aesgcmpbkdf2encryption/MainActivity.java
@@ -38,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     EditText plaintext, passphrase, ciphertext, decryptedtext;
     Button encrypt, decrypt;
 
+    String plaintextString;
+    String ciphertextBase64;
+    byte[] dataToSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         encrypt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ciphertext.setText("");
+                decryptedtext.setText("");
+                ciphertextBase64 = "";
+                dataToSave = null;
                 // get the passphrase from EditText as char array
                 int passphraseLength = passphrase.length();
                 char[] passphraseChar = new char[passphraseLength];
@@ -76,13 +84,15 @@ public class MainActivity extends AppCompatActivity {
                 int passphraseLength = passphrase.length();
                 char[] passphraseChar = new char[passphraseLength];
                 passphrase.getText().getChars(0, passphraseLength, passphraseChar, 0);
+                byte[] ciphertext = null; // todo get the ciphertext from a file
+
                 // do not run the decryption on main gui thread as it may block
                 // String decryptedtextData = doDecryptionAesGcmPbkdf2(passphraseChar, ciphertext.getText().toString());
                 // decryptedtext.setText(decryptedtextData);
                 // run the encryption in a different thread instead
                 Thread thread = new Thread(){
                     public void run(){
-                        doAesDecryption(passphraseChar, ciphertext.getText().toString());
+                        doAesDecryption(passphraseChar, ciphertext);
                     }
                 };
                 thread.start();
@@ -103,17 +113,142 @@ public class MainActivity extends AppCompatActivity {
 
     // this method is running in a thread, so don't update the ui directly
     private void doAesEncryption(char[] passphraseChar, byte[] plaintextByte) {
-        String ciphertextData = doEncryptionAesGcmPbkdf2(passphraseChar, plaintextByte);
+        String ciphertextData = base64Encoding(doEncryptionAesGcmPbkdf2(passphraseChar, plaintextByte));
         setText(ciphertext, ciphertextData);
     }
 
     // this method is running in a thread, so don't update the ui directly
-    private void doAesDecryption(char[] passphraseChar, String ciphertextBase64) {
-        String decryptedtextData = doDecryptionAesGcmPbkdf2(passphraseChar, ciphertextBase64);
+    private void doAesDecryption(char[] passphraseChar, byte[] ciphertext) {
+        String decryptedtextData = doDecryptionAesGcmPbkdf2(passphraseChar, ciphertext);
         setText(decryptedtext, decryptedtextData);
     }
 
-    private String doEncryptionAesGcmPbkdf2(char[] passphraseChar, byte[] plaintextByte) {
+    private byte[] doEncryptionAesGcmPbkdf2(char[] passphraseChar, byte[] plaintextByte) {
+        final int PBKDF2_ITERATIONS = 10000; // fixed as minimum
+        final String TRANSFORMATION_GCM = "AES/GCM/NoPadding";
+        int saltLength = 32;
+        int nonceLength = 12;
+        // generate 32 byte random salt for pbkdf2
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[saltLength];
+        secureRandom.nextBytes(salt);
+        // generate 12 byte random nonce for AES GCM
+        byte[] nonce = new byte[nonceLength];
+        secureRandom.nextBytes(nonce);
+        byte[] secretKey = new byte[0];
+        SecretKeyFactory secretKeyFactory = null;
+        // we are deriving the secretKey from the passphrase with PBKDF2 and using
+        // the hash algorithm Hmac256, this is built in from SDK >= 26
+        // for older SDKs we are using the own PBKDF2 function
+        // api between 23 - 25
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            try {
+                // uses 3rd party PBKDF function to get PBKDF2withHmacSHA256
+                // PBKDF2withHmacSHA256	is available API 26+
+                byte[] passphraseByte = charArrayToByteArray(passphraseChar);
+                secretKey = PBKDF.pbkdf2("HmacSHA256", passphraseByte, salt, PBKDF2_ITERATIONS, 32);
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+                Log.e("APP_TAG", "generateAndStoreSecretKeyFromPassphrase error: " + e.toString());
+                return null;
+            }
+        }
+        // api 26+ has HmacSHA256 available
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                KeySpec keySpec = new PBEKeySpec(passphraseChar, salt, PBKDF2_ITERATIONS, 32 * 8);
+                secretKey = secretKeyFactory.generateSecret(keySpec).getEncoded();
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey, "AES");
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(16 * 8, nonce);
+        Cipher cipher = null;
+        byte[] ciphertext = new byte[0];
+        try {
+            cipher = Cipher.getInstance(TRANSFORMATION_GCM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, gcmParameterSpec);
+            ciphertext = cipher.doFinal(plaintextByte);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // concatenating salt, nonce and ciphertext
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            outputStream.write(salt);
+            outputStream.write(nonce);
+            outputStream.write(ciphertext);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return outputStream.toByteArray();
+    }
+
+    private String doDecryptionAesGcmPbkdf2(char[] passphraseChar, byte[] ciphertextComplete) {
+        final int PBKDF2_ITERATIONS = 10000; // fixed as minimum
+        final String TRANSFORMATION_GCM = "AES/GCM/NoPadding";
+        int saltLength = 32;
+        int nonceLength = 12;
+        // split the complete ciphertext into salt, nonce and ciphertext
+        ByteBuffer bb = ByteBuffer.wrap(ciphertextComplete);
+        byte[] salt = new byte[saltLength];
+        byte[] nonce = new byte[nonceLength];
+        byte[] ciphertext = new byte[(ciphertextComplete.length - saltLength - nonceLength)];
+        bb.get(salt, 0, salt.length);
+        bb.get(nonce, 0, nonce.length);
+        bb.get(ciphertext, 0, ciphertext.length);
+        SecretKeyFactory secretKeyFactory = null;
+        byte[] secretKey = new byte[0];
+        // we are deriving the secretKey from the passphrase with PBKDF2 and using
+        // the hash algorithm Hmac256, this is built in from SDK >= 26
+        // for older SDKs we are using the own PBKDF2 function
+        // api between 23 - 25
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            try {
+                // uses 3rd party PBKDF function to get PBKDF2withHmacSHA256
+                // PBKDF2withHmacSHA256	is available API 26+
+                byte[] passphraseByte = charArrayToByteArray(passphraseChar);
+                secretKey = PBKDF.pbkdf2("HmacSHA256", passphraseByte, salt, PBKDF2_ITERATIONS, 32);
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+                Log.e("APP_TAG", "generateAndStoreSecretKeyFromPassphrase error: " + e.toString());
+                return "";
+            }
+        }
+        // api 26+ has HmacSHA256 available
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                KeySpec keySpec = new PBEKeySpec(passphraseChar, salt, PBKDF2_ITERATIONS, 32 * 8);
+                secretKey = secretKeyFactory.generateSecret(keySpec).getEncoded();
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey, "AES");
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(16 * 8, nonce);
+        Cipher cipher = null;
+        byte[] decryptedtextByte = new byte[0];
+        try {
+            cipher = Cipher.getInstance(TRANSFORMATION_GCM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, gcmParameterSpec);
+            decryptedtextByte = cipher.doFinal(ciphertext);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+            return "ERROR: The passphrase may be wrong or the ciphertext is corrupted";
+        }
+        return new String(decryptedtextByte, StandardCharsets.UTF_8);
+    }
+
+    private String doEncryptionAesGcmPbkdf2Base64(char[] passphraseChar, byte[] plaintextByte) {
         final int PBKDF2_ITERATIONS = 10000; // fixed as minimum
         final String TRANSFORMATION_GCM = "AES/GCM/NoPadding";
         int saltLength = 32;
@@ -180,7 +315,7 @@ public class MainActivity extends AppCompatActivity {
         return base64Encoding(outputStream.toByteArray());
     }
 
-    private String doDecryptionAesGcmPbkdf2(char[] passphraseChar, String ciphertextBase64) {
+    private String doDecryptionAesGcmPbkdf2FromBase64(char[] passphraseChar, String ciphertextBase64) {
         final int PBKDF2_ITERATIONS = 10000; // fixed as minimum
         final String TRANSFORMATION_GCM = "AES/GCM/NoPadding";
         int saltLength = 32;
